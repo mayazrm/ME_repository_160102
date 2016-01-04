fbc9497dcac30b869b6ec90f7dafa7c7323dc2a5
mac2393 <mac2393@columbia.edu>
2016-01-04 16:02:03 -0500

    testscript, but from within project ME

--- testscript.R
+++ testscript.R
@@ -4,3 +4,4 @@ library(ggplot2)
 options(digits=3)
 # more test lines after messing with Terminal 
 # tried to setup other repository in Terminal prompt
+# I opened this script up from within the ME project.  Let's see if i can push this line. 
